583a0162f77b9651e308de9306067784b469f623
calvin.kurniawan@tokopedia.com <calvin.kurniawan@tokopedia.com>
2019-05-06 15:32:17 +0700

    clear old repo, change repo for publish purpose only. code on bitbucket

---
+++ precache-manifest.80d79afc402a509512f799fea6b38ca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfb4a78a24159f246e38",
+    "url": "./static/css/main.133650b8.chunk.css"
+  },
+  {
+    "revision": "bfb4a78a24159f246e38",
+    "url": "./static/js/main.bfb4a78a.chunk.js"
+  },
+  {
+    "revision": "45de690a65599f33a4b1",
+    "url": "./static/js/1.45de690a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "aabfed8b2dcb061b0e5dcc65da659a57",
+    "url": "./index.html"
+  }
+];
